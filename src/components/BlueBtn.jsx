95beb377830238bac500707373b0fd8530074a86
kabi02 <108052970+kabi02@users.noreply.github.com>
2023-07-10 22:44:50 +0800

    completed layout for homepage still needs adjustments

---
+++ src/components/BlueBtn.jsx
@@ -0,0 +1,5 @@
+export default function BlueBtn({text}) {
+    return(
+        <button type="button" className="py-3 px-7 rounded-full bg-blue-500 font-open-sans font-bold text-white">{text}</button>
+    )
+}

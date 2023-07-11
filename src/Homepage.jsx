814616b1d274166b8b20a29ab82ee28d345122ee
valeriannnne <valerie.liann@gmail.com>
2023-07-11 11:31:12 +0800

    responsive homepage

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,11 +1,11 @@
 import BlueBtn from "./components/BlueBtn"
 export default function Homepage() {
     return (
-        <div className="w-full bg-[rgba(255,255,255,0.75)] font-open-sans">
+        <div className="w-full bg-[rgba(255,255,255,0.75)]  font-open-sans">
             <div className="flex flex-col items-center py-10 justify-center max-w-[80%] mx-auto gap-5"> 
-                <h1 className="text-3xl font-bold">ABOUT THIS TEST</h1>
-                <div className="flex flex-row mx-auto w-[60%] items-center justify-center">
-                    <p className="w-[70%]">
+                <h1 className="sm:text-3xl text-xl font-bold">ABOUT THIS TEST</h1>
+                <div className="flex md:flex-row flex-col mx-auto min-[1700px]:w-[60%] lg:w-[80%] items-center justify-center">
+                    <p className="sm:w-[70%] w-[90%] sm:text-base text-sm">
                     Welcome to MindCheck! As part of our commitment to help clinical evaluation and research, we created a website and evaluation that incorporates the American Psychological Association (APA)'s DSM-5-TR measures. These measures have been specifically developed to be administered during the initial patient interview and to effectively evaluate mental stability. <br /><br />
 
                     It is important to note that these assessment measures should not be used as the sole basis for making a clinical diagnosis. Instead, they are intended to be utilized as potentially valuable tools to enhance clinical decision-making. Instructions, scoring information, and interpretation guidelines are readily available to assist clinicians and users.<br /><br />

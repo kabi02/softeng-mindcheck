5c448ab6b6d81fe7aae52303e2f1eecb5f9b48b5
valeriannnne <valerie.liann@gmail.com>
2023-07-23 12:17:37 +0800

    active links

--- src/components/Li.jsx
+++ src/components/Li.jsx
@@ -1,12 +1,19 @@
-import {Link} from 'react-router-dom'
+import {Link, useLocation} from 'react-router-dom'
 
 // Used for the each links in Header.jsx
 export default function Li({text, link}) {
+    // Get the current URL
+    const location = useLocation();
+
+    // Compare the current URL with the link prop
+    const isActive = location.pathname === link;
+
     return(
         <li className='hover:scale-110 transition-transform duration-2000 cursor-pointer 
          sm:text-base text-sm
-         rounded-md max-lg:hover:bg-white max-lg:hover:text-blue-500 max-lg:hover:font-bold'>
-            <Link to={link}>
+         rounded-md max-lg:hover:bg-white max-lg:hover:text-blue-500'>
+            {/* Conditionally apply the font-bold class */}
+            <Link to={link} className={isActive ? 'font-bold' : ''}>
                 {text}
             </Link>
             
